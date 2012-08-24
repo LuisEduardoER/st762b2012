07a3d9584f6c97c0bc4a7cf0d77acd3c3b2f2cc1
anderson44444 <anderson44444@gmail.com>
2012-08-24 13:02:00 +0000

    adicionando arquivo

---
+++ src/Portifolio.java
@@ -0,0 +1,18 @@
+
+public class Portifolio {
+	private int totalDeContratos;
+	private int totalContratosAssinados;
+	private int totalImoveisVagos;
+	
+	public int getTotalDeContratos (){
+		return totalDeContratos;
+	}
+	
+	public int getTotalContratosAssinados (){
+		return totalContratosAssinados;
+	}
+	
+	public int getTotalImoveisVagos(){
+		return totalImoveisVagos;
+	}
+}
