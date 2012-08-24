1d2d33ee976f3cb68c941c6a912cf8321611753d
anderson44444@gmail.com <anderson44444@gmail.com@36a6b4a7-75f4-224d-ace2-bb54961145dc>
2012-08-24 13:59:11 +0000

    Adicionada classe de tratamento de erros

---
+++ trunk/src/TrataErro.java
@@ -0,0 +1,11 @@
+
+public class TrataErro {
+
+	private static Exception _err;
+	
+	public static void mostraErro (String Mensagem) throws Exception{
+		_err = new Exception(Mensagem);
+		throw _err;
+	}
+	
+}
