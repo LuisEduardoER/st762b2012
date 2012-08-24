4e8ac7821fa50079546de3533e7eb2ab3cab41f4
anderson44444@gmail.com <anderson44444@gmail.com@36a6b4a7-75f4-224d-ace2-bb54961145dc>
2012-08-24 14:30:05 +0000

    adicionado try/catch

--- trunk/src/Principal.java
+++ trunk/src/Principal.java
@@ -2,9 +2,13 @@ public class Principal {
 
 	public static void main(String[] args)
 	{		
-
-		Equipe e = new Equipe();		
-		e.CadastrarEquipe();
-		e.contratarFuncionario();
+       try{
+		   Equipe e = new Equipe();		
+		   e.CadastrarEquipe();
+		   e.contratarFuncionario();
+       }
+       catch (Exception erro){
+    	   System.out.printf(erro.getMessage());
+       }
 	}
 }
