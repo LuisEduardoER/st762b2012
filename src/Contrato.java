ac5a44372b593f17fc253fe0e37e07dd4d1e787a
anderson44444 <anderson44444@gmail.com>
2012-08-28 20:03:00 +0000

    çaporra nao vai por nada

--- src/Contrato.java
+++ src/Contrato.java
@@ -2,14 +2,37 @@ import java.util.Date;
 
 
 public class Contrato {
-     private int idContrato;
+     private static TrataErro erro = new TrataErro();
+	
+	 private int idContrato;
      private int valorContrato;
      private Date dataAssinatura;
-     private Inquilino inquilino;
-     private Imovel imovel;
-     private Corretor corretor;
-     private static int pIdContrato; // variavel de auto-incremento para gerra PK
+     private boolean assinado;
      
+     private Proprietario proprietario;
+     private Corretor corretor;
+     private Fiador fiador;
+     
+     private static int pIdContrato; // variavel de auto-incremento para gerar PK
+     
+     public Contrato (Corretor     nCorretor,
+    		          Proprietario nProprietario) throws Exception {
+
+    	 if (nCorretor == null){
+    		 erro.mostraErro("Corretor Invalido");
+    	 }
+    	 if (nProprietario == null){
+    		 erro.mostraErro("Proprietario Invalido");
+    	 }
+    	 
+    	 assinado = false;
+    	 corretor = nCorretor;
+    	 proprietario = nProprietario;
+    	 pIdContrato++;
+    	 idContrato = pIdContrato;
+     }
+     
+ 
      public int getIdContrato (){
     	 return idContrato;
      }
@@ -18,17 +41,50 @@ public class Contrato {
     	 return valorContrato;
      }
      
-     public void setValorContrato (int nValorContrato){
-    	 valorContrato = nValorContrato;
-     }
      
      public Date getDataAssinatura (){
     	 return dataAssinatura;
      }
      
-     public void setDataAssinatura (Date nDataAssinatura){
-    	 dataAssinatura = nDataAssinatura;
+     public void setValorContrato (int nValorContrato) throws Exception{
+    	 if (assinado){
+    		 erro.mostraErro("Contrato ja assinado");
+    	 }
+    	 valorContrato = nValorContrato;
      }
      
+     
+     public Proprietario getProprietario(){
+    	 return proprietario;
+     }
+     /*IMPLEMENTAR BY MY WAY
+     public Inquilino getInquilino(){
+    	 return inquilino;
+     }*/
+     
+     public Corretor getCorretor(){
+    	 return corretor;
+     }
+     
+    /* public Imovel getImovel(){
+    	 return imovel;
+     }*/
+     
+     public Fiador getFiador() throws Exception{
+    	 if (fiador == null){
+    		 erro.mostraErro("fiador ainda nao cadastrado");
+    	 }
+    	 return fiador;
+     }
+     
+     public void assinarContrato(){
+    	 dataAssinatura = new Date();
+    	 assinado = true;
+     }
+     
+     
+     public void modificarDadosFiador(String nome,String RG, String CPF, int rendaComprovada){
+    	 //Implementar... antes verificar detalhes do fiador, beijos
+     }
      
 }
