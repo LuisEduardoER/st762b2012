c6e84892290ad1a11cba16eea0d73b2220386700
anderson44444@gmail.com <anderson44444@gmail.com@36a6b4a7-75f4-224d-ace2-bb54961145dc>
2012-08-28 20:03:00 +0000

    çaporra nao vai por nada

--- trunk/src/Imovel.java
+++ trunk/src/Imovel.java
@@ -1,14 +1,17 @@
 
 public class Imovel {
-	private int     tipo;
-	private int     areaUtil;
-	private int     numeroQuartos;
-	private int     numeroBanheiros;
-	private int     numeroVagasGaragem;
-	private int     numeroComodos;
-	private boolean possuiAreaExterna;
-	private boolean alugado;
-	private int     idImovel;
+	private int       tipo;
+	private int       areaUtil;
+	private int       numeroQuartos;
+	private int       numeroBanheiros;
+	private int       numeroVagasGaragem;
+	private int       numeroComodos;
+	private boolean   possuiAreaExterna;
+	private boolean   alugado;
+	private int       idImovel;
+	
+	private Endereco  endereco;
+	private Inquilino inquilino;
 	/********************************************************
 	 * tipo:                                                *
 	 * 1 = Casa;                                            *
@@ -20,71 +23,72 @@ public class Imovel {
 	public int getTipo (){
 		return tipo;
 	}
-	
-    public void setTipo (int nTipo){
-    	tipo = nTipo;
-    }
-    
+
     public int getAreaUtil (){
         return areaUtil;
-    }
-    
-    public void setAreaUtil (int nAreaUtil){
-    	areaUtil = nAreaUtil;
     }
     
     public int getNumeroQuartos (){
     	return numeroQuartos;
     }
     
-    public void setNumeroQuartos (int nNumeroQuartos){
-    	numeroQuartos = nNumeroQuartos;
-    }
-    
     public int getNumeroBanheiros (){
     	return numeroBanheiros;
-    }
-    
-    public void setNumeroBanheiros (int nNumeroBanheiros){
-    	numeroBanheiros = nNumeroBanheiros;
     }
     
     public int getNumeroVagasGaragem (){
     	return numeroVagasGaragem;
     }
     
-    public void setNumeroVagasGaragem (int nNumeroVagasGaragem){
-    	numeroVagasGaragem = nNumeroVagasGaragem;
-    }
-    
     public int getNumeroComodos (){
     	return numeroComodos;
+    }
+    
+    public boolean getPossuiuAreaExterna (){
+    	return possuiAreaExterna;
+    }
+     
+    public boolean getAlugado (){
+    	return alugado;
+    }
+    
+    public int getIdImovel (){
+    	return idImovel;
+    }
+
+    
+    
+    public void setTipo (int nTipo){
+    	tipo = nTipo;
+    }
+    
+    public void setAreaUtil (int nAreaUtil){
+    	areaUtil = nAreaUtil;
+    }
+    
+    public void setNumeroQuartos (int nNumeroQuartos){
+    	numeroQuartos = nNumeroQuartos;
+    }
+    
+    public void setNumeroBanheiros (int nNumeroBanheiros){
+    	numeroBanheiros = nNumeroBanheiros;
+    }
+    
+    public void setNumeroVagasGaragem (int nNumeroVagasGaragem){
+    	numeroVagasGaragem = nNumeroVagasGaragem;
     }
     
     public void setNumeroComodos (int nNumeroComodos){
     	numeroComodos = nNumeroComodos;
     }
     
-    public boolean getPossuiuAreaExterna (){
-    	return possuiAreaExterna;
-    }
-    
     public void setPossuiuAreaExterna (boolean nPossuiuAreaExterna){
     	possuiAreaExterna = nPossuiuAreaExterna;
     }
     
-    public boolean getAlugado (){
-    	return alugado;
-    }
-    
     public void setAlugado (boolean nAlugado){
     	alugado = nAlugado;
-    }
-    public int getIdImovel (){
-    	return idImovel;
-    }
-
-
+    } 
 
 }
 
