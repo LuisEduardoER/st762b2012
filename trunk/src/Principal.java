7c2783293b2264728478994334d846fa07954d30
anderson44444@gmail.com <anderson44444@gmail.com@36a6b4a7-75f4-224d-ace2-bb54961145dc>
2012-08-28 17:54:03 +0000

    finalizada classe Endereço... classe TrataErro comentada (ou nao), Classe Contrato incompleta

--- trunk/src/Principal.java
+++ trunk/src/Principal.java
@@ -3,9 +3,9 @@ public class Principal {
 	public static void main(String[] args)
 	{		
        try{
-		   Equipe e = new Equipe();		
-		   e.CadastrarEquipe();
-		   e.contratarFuncionario();
+		   //Equipe e = new Equipe();		
+		   //e.CadastrarEquipe();
+		   //e.contratarFuncionario();
        }
        catch (Exception erro){
     	   System.out.printf(erro.getMessage());
--- trunk/src/TrataErro.java
+++ trunk/src/TrataErro.java
@@ -9,3 +9,5 @@ public class TrataErro {
 	}
 	
 }
+
+//TurnMeOnDeadMan
