6bc22b7eace2ddff9cc6b588064d1f708e3f3a5a
anderson44444@gmail.com <anderson44444@gmail.com@36a6b4a7-75f4-224d-ace2-bb54961145dc>
2012-08-29 03:48:17 +0000

    Imovel Acabado

--- trunk/src/Imovel.java
+++ trunk/src/Imovel.java
@@ -1,5 +1,7 @@
 
 public class Imovel {
+	private static TrataErro erro = new TrataErro();
+	
 	private int       tipo;
 	private int       areaUtil;
 	private int       numeroQuartos;
@@ -9,6 +11,8 @@ public class Imovel {
 	private boolean   possuiAreaExterna;
 	private boolean   alugado;
 	private int       idImovel;
+	
+	private static int pIdImovel;
 	
 	private Endereco  endereco;
 	private Inquilino inquilino;
@@ -56,9 +60,22 @@ public class Imovel {
     	return idImovel;
     }
 
+    public Endereco getEndereco(){
+    	return endereco;
+    }
+    
+    public Inquilino getInquilino() throws Exception{
+    	if (inquilino == null){
+    		erro.mostraErro("Inquilino não cadastrado");
+    	}
+    	return inquilino;
+    }
     
     
-    public void setTipo (int nTipo){
+    public void setTipo (int nTipo) throws Exception{
+    	if ((nTipo < 1)||(nTipo > 4)){
+    		erro.mostraErro("Tipo Invalido");
+    	}
     	tipo = nTipo;
     }
     
@@ -89,7 +106,41 @@ public class Imovel {
     public void setAlugado (boolean nAlugado){
     	alugado = nAlugado;
     } 
+    
+    public void setEndereco (Endereco nEndereco){
+    	endereco = nEndereco;
+    }
+    
+    public void setInquilino (Inquilino nInquilino)throws Exception{
+    	if (!alugado){
+    		erro.mostraErro("antes de incluir um inquilino, altere o status para alugado");
+    	}
+    	inquilino = nInquilino;
+    }
 
+    public Imovel(int      nTipo,
+    		      int      nAreaUtil,
+    		      int      nNumeroQuartos,
+    		      int      nNumeroBanheiros,
+    		      int      nNumeroVagasGaragem,
+    		      int      nNumeroComodos,
+    		      boolean  nPossuiuAreaExterna,
+    		      Endereco nEndereco) throws Exception{
+    	
+    	
+    	setTipo(nTipo);
+    	setAreaUtil(nAreaUtil);
+    	setNumeroQuartos(nNumeroQuartos);
+    	setNumeroBanheiros(nNumeroBanheiros);
+    	setNumeroVagasGaragem(nNumeroVagasGaragem);
+    	setNumeroComodos(nNumeroComodos);
+    	setPossuiuAreaExterna(nPossuiuAreaExterna);
+    	setEndereco(nEndereco);
+    	
+    	alugado = false;
+    	pIdImovel++;
+    	idImovel = pIdImovel;
+    }
 }
 
 
