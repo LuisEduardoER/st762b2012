e311525d275e21c1758c64eb0434cf8eda929ee0
anderson44444 <anderson44444@gmail.com>
2012-08-29 04:17:13 +0000

    contrat refinada principal iniciada

--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,36 @@
-public class Principal {
+import java.util.Scanner;
 
+public class Principal {
+	public static Scanner leitor = new Scanner(System.in);
+	
 	public static void main(String[] args)
 	{		
-       try{
-		   //Equipe e = new Equipe();		
-		   //e.CadastrarEquipe();
-		   //e.contratarFuncionario();
-       }
-       catch (Exception erro){
-    	   System.out.printf(erro.getMessage());
-       }
+      
+	   boolean sair = false;
+	   String comando;
+	   String mComando[];
+		
+	   while (!sair){
+		   System.out.printf("imobiliaria pagou morou > ");
+		   comando = leitor.next();
+		   mComando = comando.split(" ");
+		   
+		   if ((mComando[0].toUpperCase() == "HELP")||((mComando[0].toUpperCase() == "AJUDA"))){
+			   System.out.printf("sistema paga alugueis de casas \n");
+			   System.out.printf("\n");
+			   System.out.printf("Comandos \n");
+			   System.out.printf("Ajuda: Mostra essa tela\n");
+		   }else if ((mComando[0].toUpperCase() == "SAIR")||(mComando[0].toUpperCase() == "EXIT")){
+			   sair = true;
+		   }
+		   /*try{
+				  
+	       }
+	       catch (Exception erro){
+	    	   System.out.printf(erro.getMessage());
+	       } */  
+	   }
+      
+
 	}
 }
